9263e15eb2ce7070b818566cdb31efb91390d5f7
Hanna Shevliakova <evro78@gmail.com>
2025-09-03 03:18:59 +0300

    Info about scrinshot's added

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,14 @@ fetch('menu.html')
     .then(html => {
       document.querySelector('header').innerHTML = html;
     })
-    .catch(err => console.error('Меню не завантажено:', err));
+    .catch(err => console.error('Menu not loaded...', err));
+
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(html => {
+      document.querySelector('footer').innerHTML = html;
+    })
+    .catch(err => console.error('Footer not loaded:', err));
+
 
